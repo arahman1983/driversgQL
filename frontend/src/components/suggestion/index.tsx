ff73f0cbc3875c8ed91a612f68e35a287f806660
AbdelrahmanElhussiny <arahman1783@yahoo.com>
2021-12-25 13:53:08 +0200

    implement the suggestion menu UI

---
+++ frontend/src/components/suggestion/index.tsx
@@ -0,0 +1,14 @@
+// import styles from './suggestions.module.css'
+
+type Props = {
+  address : string,
+  clickHandler: (address:string) => void
+}
+
+export default function Suggestions ({address, clickHandler}:Props){
+  return(
+    <li onClick={()=>clickHandler(address)}>
+      {address}
+    </li>
+  )
+}
--- frontend/src/views/search/index.tsx
+++ frontend/src/views/search/index.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { Search } from "../../../assets/images";
+import { Suggestions } from "../../components";
 import SearchHeader from "./header";
 import styles from './searchView.module.css'
 
 export default function SearchView (){
+  const [searchTxt, setSearchTxt] = useState<string>("")
+  const [suggestionShow, setSuggestionShow] = useState<boolean>(false)
+  
+
+  const handleSearchChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setSearchTxt(e.currentTarget.value)
+    setSuggestionShow(true)
+  }
+
+  const suggestClickHandler = (address : string) => {
+    setSearchTxt(address)
+    setSuggestionShow(false)
+  }
+
   return(
-    <div className={styles.searchContainer}>
-      <SearchHeader />
-      <div className={styles.formContainer}>
-        <input type="search" className={styles.searchInput} placeholder="Pickup location" />
-        <img src={Search} alt ="search" className={styles.searchImg} />
+    <>
+      <div className={styles.searchContainer}>
+        <SearchHeader />
+        <div className={styles.formContainer}>
+          <input type="search" className={styles.searchInput} placeholder="Pickup location" value={searchTxt} onChange={handleSearchChange} />
+          <img src={Search} alt ="search" className={styles.searchImg} />
+          {
+            suggestionShow &&
+              <ul className= {styles.suggestions}>
+                <Suggestions address="text1" clickHandler = {suggestClickHandler} />
+                <Suggestions address="text3" clickHandler = {suggestClickHandler} />
+                <Suggestions address="text2" clickHandler = {suggestClickHandler} />
+              </ul>
+          }
+        </div>
       </div>
-    </div>
+      
+    </>
   )
 }
