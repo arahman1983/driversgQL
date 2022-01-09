98166e5c0771a324986d1da21fe54f5f5e3d3c89
AbdelrahmanElhussiny <arahman1783@yahoo.com>
2022-01-09 09:30:07 +0200

    test

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-// import styles from './header.module.css'
+import styles from './header.module.css'
 
 interface Props {
   title1: string
@@ -8,7 +8,7 @@ interface Props {
 
 export default function SearchHeader ({ title1, title2 }: Props): ReactElement {
   return (
-    <div className={"styles.searchHeader"}>
+    <div className={styles['searchHeader']}>
       <span>{title1}</span>
       <span>{title2}</span>
     </div>
